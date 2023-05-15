d82fa8c1e97923bd8c14364671444e11a02b7657
michealdayo64 <omotoshomicheal93@gmail.com>
2023-05-15 07:21:04 +0100

    ok

--- medical/settings.py
+++ medical/settings.py
@@ -89,15 +89,15 @@ WSGI_APPLICATION = 'medical.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}'''
+}
 
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway', 
@@ -106,7 +106,7 @@ DATABASES = {
         'HOST': 'containers-us-west-18.railway.app', 
         'PORT': '7893',
     }
-}
+}'''
 
 
 

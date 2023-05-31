5e3cb570a439743808bd8a9d3df8cf3a678d44d1
michealdayo64 <omotoshomicheal93@gmail.com>
2023-05-31 16:44:29 +0100

    ok

--- medic_app/urls.py
+++ medic_app/urls.py
@@ -1,23 +1,26 @@
 from django.urls import path
-from .views import  (index, about, blog, contact, booking_page, faq, private_policy, t_and_c,
-     fifteenMinBook, bookingDetails, bookingSummary, getAllAppointment, paymnent, theProvider)
+from .views import (index, about, blog, contact, booking_page, faq, private_policy, t_and_c, services, medPsycGyn,
+                    fifteenMinBook, bookingDetails, bookingSummary, getAllAppointment, paymnent, theProvider)
 from django.views.decorators.csrf import csrf_exempt
 
 
 urlpatterns = [
-    path('', index, name = 'index'),
-    path('about-page/', about, name = 'about'),
-    path('the-provider/', theProvider, name = 'the-provider'),
-   path('blog-page/', blog, name = 'blog'),
-   path('contact-page/', contact, name='contact'),
-   path('booking-page/', booking_page, name = 'booking'),
-   path('faq-page/', faq, name = 'faq'),
-   path('private-policy-page/', private_policy, name = 'private-policy'),
-    path('t-and-c/', t_and_c, name = 't-and-c'),
-    path('fifteen-min/', csrf_exempt(fifteenMinBook), name = 'fifteen-min'),
-    path('booking-order/', csrf_exempt(bookingDetails), name = 'booking-order'),
-    path('booking-order/<id>/', csrf_exempt(bookingDetails), name = 'booking-order-id'),
-    path('summary/<id>/', bookingSummary, name = "summary-id"),
+    path('', index, name='index'),
+    path('about-page/', about, name='about'),
+    path('the-provider/', theProvider, name='the-provider'),
+    path('blog-page/', blog, name='blog'),
+    path('contact-page/', contact, name='contact'),
+    path('booking-page/', booking_page, name='booking'),
+    path('faq-page/', faq, name='faq'),
+    path('private-policy-page/', private_policy, name='private-policy'),
+    path('t-and-c/', t_and_c, name='t-and-c'),
+    path('services/', services, name='services'),
+    path('medical-management/', medPsycGyn, name='medical-management'),
+    path('fifteen-min/', csrf_exempt(fifteenMinBook), name='fifteen-min'),
+    path('booking-order/', csrf_exempt(bookingDetails), name='booking-order'),
+    path('booking-order/<id>/', csrf_exempt(bookingDetails),
+         name='booking-order-id'),
+    path('summary/<id>/', bookingSummary, name="summary-id"),
     path('get-appointment/', getAllAppointment, name='get-appointment'),
     path('payment/<id>/', paymnent, name='payment')
 ]
--- medic_app/views.py
+++ medic_app/views.py
@@ -13,6 +13,7 @@ import threading
 
 # Create your views here.
 
+
 class EmailThread(threading.Thread):
 
     def __init__(self, email):
@@ -20,7 +21,8 @@ class EmailThread(threading.Thread):
         threading.Thread.__init__(self)
 
     def run(self):
-        self.email.send(fail_silently = False)
+        self.email.send(fail_silently=False)
+
 
 def index(request):
     ailment_list = Ailments.objects.all()
@@ -29,26 +31,28 @@ def index(request):
         print(form)
         if form.is_valid():
             form.save()
-            messages.success(request, "Form Submitted Successfully")     
-            return redirect('index')   
+            messages.success(request, "Form Submitted Successfully")
+            return redirect('index')
         else:
             WriteUsForm()
-            messages.success(request, "You need to fill the form")     
-            return redirect('index') 
+            messages.success(request, "You need to fill the form")
+            return redirect('index')
     context = {
         "ailment_list": ailment_list
     }
     return render(request, 'index.html', context)
 
+
 def booking_page(request):
     context = {}
     if request.user.is_authenticated:
         ailment_list = Ailments.objects.all()
-        context ["ailment_list"] = ailment_list
+        context["ailment_list"] = ailment_list
     else:
         messages.info(request, "You have to Login to book an appointmnt")
         return redirect("index")
     return render(request, 'view/schedule.html', context)
+
 
 """def writeUs(request):
     if request.method == "POST":
@@ -61,24 +65,24 @@ def booking_page(request):
     else:
         WriteUsForm()
         return render(request, 'view/write_us.html')"""
-    
-    
+
 
 def fifteenMinBook(request):
     data = {}
     if request.user.is_authenticated:
         user_id = request.user.id
-        user = Account.objects.get(id = user_id)
+        user = Account.objects.get(id=user_id)
         if request.method == "POST":
             user.fifteen_min_trial = True
             user.save()
             data["response"] = "15min free trial Selected"
-            return JsonResponse(data = data)
+            return JsonResponse(data=data)
     else:
         data["response"] = "User not authenticated"
         return JsonResponse(json.dumps(data), safe=False)
-    
-def bookingDetails(request, id = None):
+
+
+def bookingDetails(request, id=None):
     payload = {}
     ns = json.loads(request.body)
     user = request.user
@@ -89,11 +93,11 @@ def bookingDetails(request, id = None):
             time = ns["time"]
             phone_no = ns["phone"]
             message = ns["message"]
-            
 
             if id:
-                ailment_id = Ailments.objects.get(id = id)
-                app_id = Appointment.objects.create(user = user, ailment_id = ailment_id, phone_no = phone_no, message = message, date = date_format, appointment_time = time, is_booked = False)
+                ailment_id = Ailments.objects.get(id=id)
+                app_id = Appointment.objects.create(user=user, ailment_id=ailment_id, phone_no=phone_no,
+                                                    message=message, date=date_format, appointment_time=time, is_booked=False)
                 payload["response"] = "Appointment Order"
                 payload['app_id'] = app_id.id
                 payload['firstname'] = app_id.user.first_name
@@ -102,7 +106,8 @@ def bookingDetails(request, id = None):
                 payload['date_and_time'] = f'{app_id.date}, {app_id.appointment_time}'
                 return JsonResponse((payload), safe=False)
             else:
-                app_id = Appointment.objects.create(user = user, phone_no = phone_no, message = message, date = date_format, appointment_time = time, is_booked = False)
+                app_id = Appointment.objects.create(
+                    user=user, phone_no=phone_no, message=message, date=date_format, appointment_time=time, is_booked=False)
                 payload["response"] = "Appointment Order"
                 payload['app_id'] = app_id.id
                 payload['firstname'] = app_id.user.first_name
@@ -113,26 +118,27 @@ def bookingDetails(request, id = None):
     else:
         payload["response"] = ["User not authenticated"]
         return JsonResponse(json.dumps(payload), safe=False)
-    
+
 
 def bookingSummary(request, id):
     payload = {}
     user = request.user
     if user.is_authenticated:
         print(id)
-        appointment = Appointment.objects.get(id = id, user = user)
+        appointment = Appointment.objects.get(id=id, user=user)
         print(appointment.date, appointment.appointment_time)
         payload = {
-                "firstname": appointment.user.first_name,
-                "lastname": appointment.user.last_name,
-                "service": appointment.ailment_id.title,
-                "date_and_time": f'{appointment.date}, {appointment.appointment_time}'
-            }
-        return JsonResponse(data = payload, safe=False)
+            "firstname": appointment.user.first_name,
+            "lastname": appointment.user.last_name,
+            "service": appointment.ailment_id.title,
+            "date_and_time": f'{appointment.date}, {appointment.appointment_time}'
+        }
+        return JsonResponse(data=payload, safe=False)
     else:
         payload["response"] = "User not authenticated"
         return JsonResponse(json.dumps(payload), safe=False)
-    
+
+
 def getAllAppointment(request):
     payload = {}
     data = []
@@ -142,23 +148,22 @@ def getAllAppointment(request):
             'date': i.date,
             'time': i.appointment_time
         })
-    payload['result']=data
+    payload['result'] = data
     return JsonResponse(payload, safe=False)
-
 
 
 def faq(request):
     if request.method == "POST":
         form = WriteUsForm(request.POST or None)
-        
+
         if form.is_valid():
             form.save()
-            messages.success(request, "Form Submitted Successfully")     
-            return redirect('faq')   
+            messages.success(request, "Form Submitted Successfully")
+            return redirect('faq')
         else:
             WriteUsForm()
-            messages.success(request, "You need to fill the form")     
-            return redirect('faq') 
+            messages.success(request, "You need to fill the form")
+            return redirect('faq')
     return render(request, 'view/faq.html')
 
 
@@ -167,30 +172,44 @@ def paymnent(request, id):
     user = request.user
     if request.method == "POST":
         upload_pay = request.FILES.get('upload_pay')
-        app_id = Appointment.objects.get(id = id)
+        app_id = Appointment.objects.get(id=id)
         app_id.is_booked = True
         app_id.save()
 
-        Payment.objects.create(user = user, app_id = app_id, is_payed = True, payment_prove = upload_pay)
+        Payment.objects.create(user=user, app_id=app_id,
+                               is_payed=True, payment_prove=upload_pay)
         subject = "Appointment Booking"
-        message =  f"Dear, {user.first_name}, {user.last_name}, You have just booked an appointment Successfully for the treatment of {app_id.ailment_id.title}"
+        message = f"Dear, {user.first_name}, {user.last_name}, You have just booked an appointment Successfully for the treatment of {app_id.ailment_id.title}"
         mail_from = settings.EMAIL_HOST_USER
         mail_to = [user.email, 'omotoshomicheal93@gmail.com']
         email = EmailMessage(subject, message, mail_from, mail_to)
         EmailThread(email).start()
         messages.success(request, 'You have payed Successful')
         return redirect('index')
-    #print(app_id)
+    # print(app_id)
     return render(request, 'view/payment.html')
+
 
 def theProvider(request):
     return render(request, 'view/the_provider.html')
 
+
+def services(request):
+    return render(request, 'view/services.html')
+
+
+def medPsycGyn(request):
+    ailment_list = Ailments.objects.all()
+    return render(request, 'view/med_psy_gyn.html', {'ailment_list': ailment_list})
+
+
 def blog(request):
     return render(request, 'view/blog.html')
 
+
 def about(request):
     return render(request, 'view/about.html')
+
 
 def contact(request):
     return render(request, 'view/contact.html')
@@ -199,6 +218,6 @@ def contact(request):
 def private_policy(request):
     return render(request, 'view/private_policy.html')
 
+
 def t_and_c(request):
     return render(request, 'view/t_and_c.html')
-
